5817fff0da7d77b4c2a5a70d2e6d3473725f6b50
zetpages <akimov_z@auca.kg>
2022-03-07 04:06:57 +0600

    completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 
@@ -7,12 +9,47 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.username,
-        password: req.body.username
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString()
     });
 
     try {
         const savedUser = await newUser.save()
         res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wron credentials!");
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const passwordOriginal = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        passwordOriginal !== req.body.password && res.status(401).json("Wron credentials!");
+
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json({ ...others, accessToken });
     } catch (err) {
         res.status(500).json(err);
     }
